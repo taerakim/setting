cd9ea28b2a7f84de2770293d6ff2c807d7f82855
taerankim <taetaeee02@gmail.com>
2025-01-15 20:20:07 +0900

    feat: init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+echo -en "\033[32;7m execute init.sh \033[0m\n"
+
 # preference
 if [ ! -d $GOIN/setting ]; then
+  echo "setting clone"
   git clone https://github.com/taerakim/setting $GOIN/setting
   if [ -f $GOIN/setting/init.sh ]; then
     bash $GOIN/setting/preference.sh
   fi
+else
+  echo "setting pull"
+  cd $GOIN/setting
+  git pull
 fi
 
 # obsidian
 if [ ! -d $OBSI ]; then
-  git clone https://github.com/taerakim/obsi $OBSI
+  echo "obsi clone"
+  git clone https://github.com/taerakim/obsidian $OBSI
+else
+  echo "obsi pull"
+  cd $OBSI
+  git pull
 fi
 if [ ! -f $OBSI/mutaee_obsi/rule.md ]; then
-  git clone https://github.com/mutaee/mutaee_obsi $OBSI/.
+  if [ -d $OBSI/mutaee_obsi ]; then
+    rmdir $OBSI/mutaee_obsi
+  fi
+  echo "mutaee clone"
+  git clone https://github.com/mutaee/mutaee_obsi $OBSI/mutaee_obsi
 fi
