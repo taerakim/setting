ed39f007612c0ef39a9e3e6108dde7bc647ffb7e
taerankim <taetaeee02@gmail.com>
2025-01-15 20:16:33 +0900

    fix: edit order in preference.sh

--- preference.sh
+++ preference.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
+echo -en "\033[32;7m execute preference.sh \033[0m\n"
+
 PREF_KEYBOARD="com.apple.preference.keyboard"
 PREF_MOUSE="com.apple.preference.mouse"
 PREF_DISPLAYS="com.apple.preference.displays"
 
-osascript <<EOD
-	tell application "System Preferences"
-		activate
-		reveal anchor "KeyboardTab" of pane id "$PREF_KEYBOARD"
-		return --for remove 'missing value'
-	end tell
-EOD
-echo "Keyboard setting"
-
-sleep 3
+if [ "RE" != "true" ]; then
+  sleep 5
+fi
 
 osascript <<EOD
 	tell application "System Preferences"
@@ -23,6 +18,17 @@ osascript <<EOD
 	end tell
 EOD
 echo "Keyboard Tab key setting"
+
+sleep 3
+
+osascript <<EOD
+	tell application "System Preferences"
+		activate
+		reveal anchor "KeyboardTab" of pane id "$PREF_KEYBOARD"
+		return --for remove 'missing value'
+	end tell
+EOD
+echo "Keyboard setting"
 
 sleep 3
 
